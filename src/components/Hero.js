c2dcd31972cf60a0c84f5558f5e54377b3d9dd5d
Chris <chris.ware.dev@gmail.com>
2021-11-14 23:26:51 -0700

    Hero Section

---
+++ src/components/Hero.js
@@ -0,0 +1,23 @@
+import styles from "../../styles/Hero.module.css";
+import Button from "./Button";
+
+function Hero() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h1>LIVE MAS</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fuga
+          commodi id doloribus pariatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <div className={styles.buttonContainer}>
+          <Button type='primaryBtn' label='Menu' href='/' />
+          <Button type='secondaryBtn' label='Locations' href='/' />
+          <Button type='tertiaryBtn' label='Order Now' href='/' />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Hero;
